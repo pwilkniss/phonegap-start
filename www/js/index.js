b5ca173d19bfd512aed9af67cb77b32b55072c5d
Peter <peter@pwang.co>
2016-04-11 19:38:35 -0700

    add ct config

--- www/js/index.js
+++ www/js/index.js
@@ -34,6 +34,7 @@ var app = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
+        CleverTap.setDebugLevel(1277182231);
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
